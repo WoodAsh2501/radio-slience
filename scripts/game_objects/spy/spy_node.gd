1e57123a978586fe09e6fc7c15cea0d7a2885560
WoodAsh <woodash830@outlook.com>
2025-06-01 01:12:31 +0800

    ✨ feat(spy): continus emit mouse inside signal to handle interaction

--- scripts/game_objects/spy/spy_node.gd
+++ scripts/game_objects/spy/spy_node.gd
@@ -2,13 +2,29 @@ extends Area2D
 
 @onready var state_machine = get_node('../StateMachine')
 
-func _on_mouse_entered() -> void:
+signal mouse_still_inside
+
+func _ready() -> void:
+	connect("mouse_still_inside", _on_mouse_still_inside)
+
+func _process(_delta) -> void:
+	var mouse_position = get_global_mouse_position()
+	if mouse_position.distance_to(global_position) < 16:
+		emit_signal("mouse_still_inside")
+
+func hover_handler():
 	if not state_machine.is_state("Idle"):
 		return
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		state_machine.spy_switch_to("Selected")
 	else:
 		state_machine.spy_switch_to("Hovering")
+
+func _on_mouse_still_inside() -> void:
+	hover_handler()
+
+func _on_mouse_entered() -> void:
+	hover_handler()
 
 func _on_mouse_exited() -> void:
 	if state_machine.get_state_name() in ["Hovering", "Selected"]:
